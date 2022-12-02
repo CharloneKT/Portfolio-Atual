0b34d41675f30555767a0225b71d6ad223a6a603
Charlone Knupp Torres <loneknupp@gmail.com>
2022-12-02 13:41:38 -0300

    Add files via upload

---
+++ js/menumobile.js
@@ -0,0 +1,26 @@
+const hamburguer = document.querySelector('#checkboxMenu');
+const mobileMenu = document.querySelector('.menuMobile');
+const links = document.querySelectorAll('.menuMobile ul li a');
+const logo = document.querySelector('.logo a');
+
+hamburguer.addEventListener('click', () => {
+   
+  mobileMenu.classList.toggle('is-open');
+
+});
+
+
+for (i = 0; i < links.length; i++) {
+  links[i].onclick = function() {
+
+  hamburguer.checked = false;
+  mobileMenu.classList.remove('is-open');
+  }
+};
+
+logo.addEventListener('click', () => {
+
+  hamburguer.checked = false;
+  mobileMenu.classList.remove('is-open');
+
+});
